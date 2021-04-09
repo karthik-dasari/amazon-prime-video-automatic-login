e90c85e3bbfef0b5a22212b143db5029879efc74
Karthik Dasari <67855672+karthik-dasari@users.noreply.github.com>
2021-04-09 22:50:40 +0530

    Add files via upload

---
+++ automation prime account .py
@@ -0,0 +1,11 @@
+import selenium
+from selenium import webdriver
+driver=webdriver.Chrome()
+driver.get('https://primevideo.com')
+driver.maximize_window()
+driver.find_element_by_class_name('_1NNx6V').click()
+driver.find_element_by_xpath('//*[@id="ap_email"]').send_keys('ksr115487@gmail.com')
+driver.find_element_by_xpath('//*[@id="ap_password"]').send_keys('11081978s')
+driver.find_element_by_class_name('a-button-input').click()
+driver.find_element_by_xpath('//*[@id="aiv-cl-main-middle"]/div/div[3]/div/div[3]/div/div[2]/div/div/ul/li[3]/div/a/div').click()
+driver.find_element_by_xpath('//*[@id="aiv-cl-main-middle"]/div/div[3]/div/div[1]/div/div[1]/div[1]/div/a').click()
